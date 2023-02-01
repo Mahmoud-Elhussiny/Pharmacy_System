8f270e26327e225a7b2330362509bf05aded95cd
Mahmoud-Elhussiny <mahmoudelhussiny1998@gmail.com>
2023-02-01 10:49:41 +0200

    change_baseresponse

--- Pharmacy System_V1/Pharmacy_SYS/Pharmacy.Application/Masseges/BaseResponse.cs
+++ Pharmacy System_V1/Pharmacy_SYS/Pharmacy.Application/Masseges/BaseResponse.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace Pharmacy.Application.Masseges
 {
-    public abstract class BaseRessponse:BaseMasseges
+    public abstract class BaseResponse:BaseMasseges
     {
-        public BaseRessponse() {  }
-        public BaseRessponse(Guid correlationId):base()
+        public BaseResponse() {  }
+        public BaseResponse(Guid correlationId):base()
         {
             _correlationId = correlationId;
         }
